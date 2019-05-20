2a5ec6ba25e8255eef58c17e67fc10736d8ebefb
PepitoSama <etienne.saimond@gmail.com>
2019-05-20 20:41:55 +0200

    Added Triggers

--- TRIGGER.sql
+++ TRIGGER.sql
@@ -15,3 +15,46 @@ CREATE TRIGGER deleteUserVideos
   ON "Playlists"
   FOR EACH ROW
   EXECUTE PROCEDURE deleteUserVideos();
+
+CREATE OR REPLACE FUNCTION deleteUserPlaylist() RETURNS trigger AS
+$BODY$
+DECLARE
+  nb integer;
+  BEGIN
+    DELETE
+    FROM "Playlists"
+    WHERE "Playlists"."UserUserId" IS NULL;
+    RETURN null;
+  END;
+$BODY$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER deleteUserPlaylist
+  AFTER DELETE
+  ON "Users"
+  FOR EACH ROW
+  EXECUTE PROCEDURE deleteUserPlaylist();
+
+CREATE OR REPLACE FUNCTION updateVideoNumber() RETURNS trigger AS
+$BODY$
+DECLARE
+  BEGIN
+    UPDATE "Playlists"
+    SET "playlistVideoNbr" = subquery.videoNbr
+    FROM (
+      SELECT "PlaylistPlaylistId", count(*) as videoNbr
+      FROM "Videos", "Playlists"
+      WHERE "Videos"."PlaylistPlaylistId" = "Playlists"."playlistId"
+      GROUP BY "PlaylistPlaylistId"
+    ) AS subquery
+    WHERE "Playlists"."playlistId" = subquery."PlaylistPlaylistId";
+    RETURN null;
+  END;
+$BODY$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER updateVideoNumber
+  AFTER INSERT OR UPDATE
+  ON "Videos"
+  FOR EACH ROW
+  EXECUTE PROCEDURE updateVideoNumber();
